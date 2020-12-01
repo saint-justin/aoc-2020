9205d41cdf808b2035badfc6bedb396d3f2ed63a
Justin Vaughn <jtv6445@g.rit.edu>
2020-12-01 12:20:22 -0500

    Added '!'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const inputs = require('./inputs/day1.js');
 const solutions = require('./solutions');
 
-// Solutions for Day 1
+// Solutions for Day 1!
 // const d1Input = inputs.day1.split('\n').map((e) => parseInt(e, 10));
 // console.log(`Day 1 Part 1: ${solutions.d1p1(d1Input)}`);
 // console.log(`Day 1 Part 2: ${solutions.d1p1(d1Input)}`);
 
-// Solutions for Day 2
+// Solutions for Day 2!
 const d2Input = inputs.day2.split('\n');
 console.log(`Day 2 Part 1: ${solutions.d1p2(d2Input)}`);
 console.log(`Day 2 Part 2: ${solutions.d1p2(d2Input)}`);
